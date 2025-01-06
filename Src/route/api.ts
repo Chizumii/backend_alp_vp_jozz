f55bb355d826a9f8ba859703c7e7f43e7d71045b
Alkupa01 <albertkurniawan383@gmail.com>
2025-01-06 08:09:45 +0700

    api

--- Src/route/api.ts
+++ Src/route/api.ts
@@ -1,14 +1,7 @@
 import express from "express"
-import { authMiddleware } from "../middleware/auth-middleware"
 import { UserController } from "../controller/user-controller"
 
 export const apiRouter = express.Router()
-apiRouter.use(authMiddleware)
+apiRouter.post("/api/register", UserController.register);
+apiRouter.post("/api/login", UserController.login);
 
-apiRouter.get("/", (req, res) => {
-    res.send("Hello World!")
-})
-
-apiRouter.post("/api/register", UserController.register)
-apiRouter.post("/api/login", UserController.login)
-apiRouter.post("/api/logout", UserController.logout)
